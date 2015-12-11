c17dec2a62e26313a00f6b2fff7f69fe2f88e2e3
Evan Chan <velvia@gmail.com>
2015-12-11 18:49:31 +0800

    Make nbits a ushort instead of ubyte

--- schema/flatbuffers/gen-java/org/velvia/filo/vector/DataInfo.java
+++ schema/flatbuffers/gen-java/org/velvia/filo/vector/DataInfo.java
@@ -11,13 +11,14 @@ import com.google.flatbuffers.*;
 public final class DataInfo extends Struct {
   public DataInfo __init(int _i, ByteBuffer _bb) { bb_pos = _i; bb = _bb; return this; }
 
-  public int nbits() { return bb.get(bb_pos + 0) & 0xFF; }
-  public boolean signed() { return 0!=bb.get(bb_pos + 1); }
+  public int nbits() { return bb.getShort(bb_pos + 0) & 0xFFFF; }
+  public boolean signed() { return 0!=bb.get(bb_pos + 2); }
 
   public static int createDataInfo(FlatBufferBuilder builder, int nbits, boolean signed) {
-    builder.prep(1, 2);
+    builder.prep(2, 4);
+    builder.pad(1);
     builder.putBoolean(signed);
-    builder.putByte((byte)(nbits & 0xFF));
+    builder.putShort((short)(nbits & 0xFFFF));
     return builder.offset();
   }
 };
