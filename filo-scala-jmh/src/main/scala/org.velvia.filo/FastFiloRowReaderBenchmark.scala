12434e774be39c601405ee8c95a3d58fa458ac5e
Evan Chan <velvia@gmail.com>
2016-02-01 14:53:29 -0800

    Add benchmark to time FastFiloRowReader creation

---
+++ filo-scala-jmh/src/main/scala/org.velvia.filo/FastFiloRowReaderBenchmark.scala
@@ -0,0 +1,45 @@
+package org.velvia.filo
+
+import java.sql.Timestamp
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.{Mode, State, Scope}
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import scalaxy.loops._
+import scala.language.postfixOps
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * Measures the speed of creating a FastFiloRowReader, and especially parsing the
+ * chunks into FiloVectors.
+ *
+ * For a description of the JMH measurement modes, see
+ * https://github.com/ktoso/sbt-jmh/blob/master/src/sbt-test/sbt-jmh/jmh-run/src/main/scala/org/openjdk/jmh/samples/JMHSample_02_BenchmarkModes.scala
+ */
+@State(Scope.Thread)
+class FastFiloRowReaderBenchmark {
+  import VectorReader._
+
+  // Ok, create an IntColumn and benchmark it.
+  val numValues = 10000
+
+  val randomInts = (0 until numValues).map(i => util.Random.nextInt)
+  val randomLongs = randomInts.map(_.toLong)
+  val randomTs = randomLongs.map(l => new Timestamp(l))
+
+  val chunks = Array(VectorBuilder(randomInts).toFiloBuffer,
+                     VectorBuilder(randomLongs).toFiloBuffer,
+                     VectorBuilder(randomTs).toFiloBuffer)
+  val clazzes = Array[Class[_]](classOf[Int], classOf[Long], classOf[Timestamp])
+
+  // According to @ktosopl, be sure to return some value if possible so that JVM won't
+  // optimize out the method body.  However JMH is apparently very good at avoiding this.
+  // fastest loop possible using FiloVectorApply method
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def createFastFiloRowReader(): RowReader = {
+    new FastFiloRowReader(chunks, clazzes)
+  }
+}
